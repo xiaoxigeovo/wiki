e334f5ef1234b95921b4f5863aaeca20bbff8910
lujingxi <lujingxi7@qq.com>
2021-07-07 02:05:00 +0800

    2-7 增加hello测试接口test.http

--- src/main/java/com/lujingxi/wiki/controller/TestController.java
+++ src/main/java/com/lujingxi/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.lujingxi.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,10 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "hello,world,post" + name;
     }
 }
