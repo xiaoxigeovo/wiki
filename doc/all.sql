3ed94adde1fa5d375c5e9885a8e976152d5f03d8
lujingxi <lujingxi7@qq.com>
2021-07-07 02:51:16 +0800

    3-2 数据库的准备，添加all.sql

---
+++ doc/all.sql
@@ -0,0 +1,68 @@
+drop table if exists `test`;
+create table `test`
+(
+    `id`         bigint      not null comment 'id',
+    `name`       varchar(50) not null comment '名称',
+    `password`       varchar(50) not null comment '密码',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
+
+
+drop table if exists `doc`;
+create table `doc`
+(
+    `id`         bigint      not null comment 'id',
+    `ebook_id`   bigint      not null default 0 comment '电子书id',
+    `parent`     bigint      not null default 0 comment '父id',
+    `name`       varchar(50) not null comment '名称',
+    `sort`       int comment '顺序',
+    `view_count` int                  default 0 comment '阅读数',
+    `vote_count` int                  default 0 comment '点赞数',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='文档';
+
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count)
+values (1, 1, 0, '文档1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count)
+values (2, 1, 1, '文档1.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count)
+values (3, 1, 0, '文档2', 2, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count)
+values (4, 1, 3, '文档2.1', 1, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count)
+values (5, 1, 3, '文档2.2', 2, 0, 0);
+insert into `doc` (id, ebook_id, parent, name, sort, view_count, vote_count)
+values (6, 1, 5, '文档2.2.1', 1, 0, 0);
+
+drop table if exists `user`;
+create table `user`
+(
+    `id`         bigint      not null comment 'ID',
+    `login_name` varchar(50) not null comment '登陆名',
+    `name`       varchar(50) comment '昵称',
+    `password`   char(32)    not null comment '密码',
+    primary key (`id`),
+    unique key `login_name_unique` (`login_name`)
+) engine = innodb
+  default charset = utf8mb4 comment ='用户';
+
+insert into `user`(`id`, `login_name`, `name`, `password`)
+values (1, 'test', '测试', 'test');
+
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot`
+(
+    `id`            bigint auto_increment not null comment 'id',
+    `ebook_id`      bigint                not null default 0 comment '电子书id ',
+    `date`          date                  not null comment '快照日期',
+    `view_count`    int                   not null default 0 comment '阅读数',
+    `vote_count`    int                   not null default 0 comment '点赞数',
+    `view_increase` int                   not null default 0 comment '阅读增长',
+    `vote_increase` int                   not null default 0 comment '点赞增长',
+    primary key (`id`),
+    unique key `ebook_id_date_unique` (`ebook_id`,`date`)
+) engine = innodb
+  default charset = utf8mb4 comment ='电子书快照表';
+
