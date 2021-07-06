6aff83296271f2b9d314d02a0646ca951ce2a9d5
lujingxi <lujingxi7@qq.com>
2021-07-07 05:03:35 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/lujingxi/wiki/service/EbookService.java
+++ src/main/java/com/lujingxi/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.lujingxi.wiki.service;
 
 import com.lujingxi.wiki.domain.Ebook;
+import com.lujingxi.wiki.domain.EbookExample;
 import com.lujingxi.wiki.mapper.EbookMapper;
+import com.lujingxi.wiki.request.EbookReq;
+import com.lujingxi.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,19 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return respList;
     }
 }
