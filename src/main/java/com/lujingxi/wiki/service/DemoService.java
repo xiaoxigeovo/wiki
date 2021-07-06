05dc1636c89eddc1c6b18fcb4d475373ff9424f9
lujingxi <lujingxi7@qq.com>
2021-07-07 04:19:20 +0800

    3-5 增加了demo表查询

---
+++ src/main/java/com/lujingxi/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.lujingxi.wiki.service;
+
+import com.lujingxi.wiki.domain.Demo;
+import com.lujingxi.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
