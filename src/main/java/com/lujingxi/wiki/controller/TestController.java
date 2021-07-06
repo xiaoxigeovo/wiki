ce4c07d4fb087e459232108daef3b560eeb9ed96
lujingxi <lujingxi7@qq.com>
2021-07-07 02:25:13 +0800

    2-9 集成热部署

--- src/main/java/com/lujingxi/wiki/controller/TestController.java
+++ src/main/java/com/lujingxi/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.lujingxi.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello}")
+    private String testHello;
+
     /**
      * GET,POST,PUT,DELETE
-     * @return
+     * @return string
      */
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World"+testHello;
     }
 
     @PostMapping("/hello/post")
