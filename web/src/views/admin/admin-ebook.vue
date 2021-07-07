eca320b4c5ec6a01cb2963d8174c996bc2d1c807
lujingxi <lujingxi7@qq.com>
2021-07-07 15:59:21 +0800

    6-9 增加雪花算法工具类，完成电子书新增功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,6 +3,11 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <p>
+        <a-button type="primary" @click="add()" size="large">
+          新增
+        </a-button>
+      </p>
       <a-table
           :columns="columns"
           :row-key="record => record.id"
@@ -174,6 +179,14 @@ export default defineComponent({
       ebook.value = record
     };
 
+    /**
+     * 增加
+     */
+    const add = (record: any) => {
+      modalVisible.value = true;
+      ebook.value = {};
+    };
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -190,6 +203,7 @@ export default defineComponent({
       handleTableChange,
 
       edit,
+      add,
 
       ebook,
       modalVisible,
