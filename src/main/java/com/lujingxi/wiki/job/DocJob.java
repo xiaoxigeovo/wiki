4d3f2e5bebf196c26533529fc932ccf423145426
lujingxi <lujingxi7@qq.com>
2021-07-07 22:44:47 +0800

    10-6 增加定时器，定时执行电子书信更新SQL,增加日志

---
+++ src/main/java/com/lujingxi/wiki/job/DocJob.java
@@ -0,0 +1,30 @@
+package com.lujingxi.wiki.job;
+
+import com.lujingxi.wiki.service.DocService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class DocJob {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
+
+    @Resource
+    private DocService docService;
+
+    /**
+     * 每30每秒更新电子书信息
+     */
+    @Scheduled(cron = "5/30 * * * * ?")
+    public void cron() {
+        LOG.info("更新电子书下的文档数据开始");
+        long start = System.currentTimeMillis();
+        docService.updateEbookInfo();
+        LOG.info("更新电子书下的文档数据结束，耗时：{}毫秒",System.currentTimeMillis() - start);
+    }
+
+}
