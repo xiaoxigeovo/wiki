a2d0db0e99cd6a3af5e7b28532825d8fa98034be
lujingxi <lujingxi7@qq.com>
2021-07-07 18:51:59 +0800

    7-8 点击二级分类时，显示该分类下的电子书（暂未实现）

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -284,12 +284,6 @@ export default defineComponent({
 
     onMounted(() => {
       handleQueryCategory();
-      handleQuery({
-        page: 1,
-        size: pagination.value.pageSize,
-
-      });
-
     });
 
     return {
